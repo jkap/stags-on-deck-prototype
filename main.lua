e54c0112ff176e70ed8b03284e41167d6bff6f26
Josh Kaplan <joshbrentkaplan@gmail.com>
2013-02-04 10:33:43 -0600

    Initial commit

---
+++ main.lua
@@ -0,0 +1,104 @@
+function love.load()
+  leftKeys = {
+    "q", "w", "e", "r", "t",
+    "a", "s", "d", "f", "g",
+    "z", "x", "c", "v"
+  }
+  rightKeys = {
+    "y", "u", "i", "o", "p",
+    "h", "j", "k", "l", ";",
+    "b", "n", "m", ","
+  }
+
+  love.physics.setMeter(64)
+  world = love.physics.newWorld(0, 9.81*64, true)
+
+  font = love.graphics.newFont(30)
+  love.graphics.setFont(font)
+
+  state = "playing"
+
+  objects = {}
+
+  objects.ground = {}
+  objects.ground.body = love.physics.newBody(world, 650/2, 650-50/2)
+  objects.ground.shape = love.physics.newRectangleShape(650,50)
+  objects.ground.fixture = love.physics.newFixture(objects.ground.body, objects.ground.shape)
+
+  player1 = {}
+  player1.body = love.physics.newBody(world, 100, 650/2, "dynamic")
+  player1.shape = love.physics.newRectangleShape(0,0,50,50)
+  player1.fixture = love.physics.newFixture(player1.body, player1.shape, 1)
+  player1.fixture:setRestitution(0)
+  player1.name = "Player 1"
+
+  player2 = {}
+  player2.body = love.physics.newBody(world, 650-100, 650/2, "dynamic")
+  player2.shape = love.physics.newRectangleShape(0,0,50,50)
+  player2.fixture = love.physics.newFixture(player2.body, player2.shape, 1)
+  player2.fixture:setRestitution(0)
+  player2.name = "Player 2"
+
+  setInitialKeys()
+
+  startTime = love.timer.getTime()
+
+  love.graphics.setMode(650,650,false,true,0)
+end
+
+
+function love.update(dt)
+  world:update(dt)
+  time = love.timer.getTime() - startTime
+  if love.keyboard.isDown(player1.key) then
+    player1.body:applyForce(10000,0)
+    repeat
+      if math.random(10) > 8 then
+        player1.key = rightKeys[math.random(#rightKeys)]
+      else
+        player1.key = leftKeys[math.random(#leftKeys)]
+      end
+    until player1.key ~= player2.key
+  end
+  if love.keyboard.isDown(player2.key) then
+    player2.body:applyForce(-10000,0)
+    repeat
+      if math.random(10) > 8 then
+        player2.key = leftKeys[math.random(#leftKeys)]
+      else
+        player2.key = rightKeys[math.random(#rightKeys)]
+      end
+    until player2.key ~= player1.key
+  end
+  if player1.body:getX() < 0 then
+    win(player2)
+  end
+  if player2.body:getX() > 650 then
+    win(player1)
+  end
+end
+
+function win(winning_player)
+  print(winning_player.name.." wins after "..time)
+  player1.body:setLinearVelocity(0,0)
+  player2.body:setLinearVelocity(0,0)
+  player1.body:setPosition(100, 650/2)
+  player2.body:setPosition(650-100, 650/2)
+  startTime = love.timer.getTime()
+  setInitialKeys()
+end
+
+function setInitialKeys()
+  player1.key = leftKeys[math.random(#leftKeys)]
+  player2.key = rightKeys[math.random(#rightKeys)]
+end
+
+function love.draw()
+  love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape:getPoints()))
+  love.graphics.polygon("fill", player1.body:getWorldPoints(player1.shape:getPoints()))
+  love.graphics.polygon("fill", player2.body:getWorldPoints(player2.shape:getPoints()))  
+
+  love.graphics.print(player1.key, 100, 100)
+  love.graphics.print(player2.key, 550, 100)
+  love.graphics.print(time, 250, 100)
+end
